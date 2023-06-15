c41ce9b380b72caad8ec3568dd62f8de1990cd6b
Pedro Miguel <pedromrap@gmail.com>
2023-06-15 12:26:46 -0300

    fix(about): update about

--- src/components/Sections/About/index.tsx
+++ src/components/Sections/About/index.tsx
@@ -23,18 +23,26 @@ const About = () => {
 
       <Text mt={3} fontSize="xl" fontWeight="600" textTransform="uppercase">
         <Highlight
-          query="pedromrap@gmail.com"
+          query="pedro.miigueel7@gmail.com"
           styles={{ color: blue, textDecoration: 'underline' }}
         >
-          (61 ) 99520-0297 | (61) 3037-2653 · Brasília, DF · pedromrap@gmail.com
+          (61) 99520-0297 | (61) 3037-2653 · Brasília, DF ·
+          pedro.miigueel7@gmail.com
         </Highlight>
       </Text>
 
       <Text mt={6} fontSize="lg" maxWidth="1200px" width="100%">
-        Desenvolvedor React com experiência nas principais tecnologias do
-        mercado como: React Hooks, API Rest, Typescript, Git e SASS. Entusiasta
-        na área de desenvolvimento web, tenho facilidade em aprender novas
-        tecnologias, sempre em busca de novas experiências.
+        Desenvolvedor front-end com experiência nas principais tecnologias do
+        mercado, incluindo React Hooks, API Rest, Typescript, Git Flow, Sass e
+        Styled components. Sou um entusiasta na área de desenvolvimento web
+        mobile, estou sempre em busca de novas experiências. Possuo facilidade
+        em aprender novas tecnologias e acredito que a atualização constante é
+        fundamental para o desenvolvimento de soluções web inovadoras e de alta
+        qualidade. Estou comprometido em criar interfaces de usuário incríveis e
+        otimizadas, trabalhando em equipe para atender às necessidades do
+        projeto. Busco novas oportunidades que me permitam aplicar minhas
+        habilidades e continuar aprendendo e evoluindo como desenvolvedor
+        front-end.
       </Text>
 
       <Flex alignItems="center" gap={6} mt={6}>
