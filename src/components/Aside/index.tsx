54a179d2dc0a7fbb1e7c9a158f600f1986974312
Pedro Miguel <pedromrap@gmail.com>
2023-06-15 12:24:36 -0300

    fix: fix animation flickering on first render

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,66 +1,60 @@
 import React from 'react';
-import { Avatar, Box, Flex, useColorModeValue } from '@chakra-ui/react';
+import { Avatar, Box, Flex, Slide, useColorModeValue } from '@chakra-ui/react';
 
-import { motion } from 'framer-motion';
 import { ToggleThemeButton } from 'components/ToggleThemeButton';
 import { data, links } from './content';
 
 const Aside = () => {
-  const MotionFlex = motion(Flex);
-
   const bg = useColorModeValue('blue.700', 'blue.900');
 
   return (
-    <MotionFlex
-      initial={{ x: -350, opacity: 0 }}
-      animate={{ x: 0, opacity: 1 }}
-      transition={{
-        default: { ease: 'linear' }
-      }}
-      maxWidth="350px"
-      w="100%"
-      bg={bg}
-      position="sticky"
-      top={0}
-      bottom={0}
-      h="100vh"
-      alignItems="center"
-      justifyContent="center"
-      gap={8}
-      as="aside"
-      flexDirection="column"
-      padding={5}
-      textAlign="center"
-    >
-      <ToggleThemeButton />
-      <Box width="160px" height="160px">
-        <Avatar
-          size="full"
-          name={data.name}
-          src={data.avatar}
-          border="0.5rem solid rgba(255, 255, 255, 0.205)"
-          bg="rgba(255, 255, 255, 0.205)"
-        />
-      </Box>
+    <Slide direction="left" in={true} style={{ zIndex: 10 }}>
+      <Flex
+        maxWidth="350px"
+        w="100%"
+        bg={bg}
+        position="sticky"
+        top={0}
+        bottom={0}
+        h="100vh"
+        alignItems="center"
+        justifyContent="center"
+        gap={8}
+        as="aside"
+        flexDirection="column"
+        padding={5}
+        textAlign="center"
+      >
+        <ToggleThemeButton />
+        <Box width="160px" height="160px">
+          <Avatar
+            size="full"
+            name={data.name}
+            src={data.avatar}
+            border="0.5rem solid rgba(255, 255, 255, 0.205)"
+            bg="rgba(255, 255, 255, 0.205)"
+          />
+        </Box>
 
-      <Flex as="nav" flexDirection="column" gap={4} fontSize="lg">
-        {links.map(({ label, href }) => (
-          <Box
-            key={href}
-            as="a"
-            href={href}
-            color="white"
-            opacity={0.5}
-            textTransform="uppercase"
-            fontWeight={600}
-            transition="all 0.3s ease-in-out"
-            _hover={{ opacity: 1 }}
-          >
-            {label}
-          </Box>
-        ))}
+        <Flex as="nav" flexDirection="column" gap={4} fontSize="lg">
+          {links.map(({ label, href }) => (
+            <Box
+              key={href}
+              as="a"
+              href={href}
+              color="white"
+              opacity={0.5}
+              textTransform="uppercase"
+              fontWeight={600}
+              transition="all 0.3s ease-in-out"
+              _hover={{ opacity: 1 }}
+            >
+              {label}
+            </Box>
+          ))}
+        </Flex>
       </Flex>
-    </MotionFlex>
+    </Slide>
   );
 };
 
